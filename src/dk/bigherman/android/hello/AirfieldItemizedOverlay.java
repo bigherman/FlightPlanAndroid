efa245762d2f55a4a6ef6576274f8895b26f2289
Martin Mortensen <msmortensen@gmail.com>
2012-05-11 10:02:27 +0200

    Airfields will now load on double tap, instead of every time you move.

--- src/dk/bigherman/android/hello/AirfieldItemizedOverlay.java
+++ src/dk/bigherman/android/hello/AirfieldItemizedOverlay.java
@@ -16,8 +16,9 @@ public class AirfieldItemizedOverlay extends ItemizedOverlay<OverlayItem>
 {
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	private Context mContext;
-	private int curLong = 0;
-	private int curLat = 0;
+	//private int curLong = 0;
+	//private int curLat = 0;
+	private long lastTouchTime = -1;
 	public AirfieldItemizedOverlay(Drawable defaultMarker) 
 	{
 		super(boundCenterBottom(defaultMarker));
@@ -68,35 +69,37 @@ public class AirfieldItemizedOverlay extends ItemizedOverlay<OverlayItem>
     {
     	Log.i("onTouchEvent", ""+mapView.getZoomLevel());
     	super.onTouchEvent(event, mapView);
-    	if (event.getAction() == MotionEvent.ACTION_UP) {
-    		GeoPoint geoPoint = mapView.getProjection().fromPixels(0,0);
-    		double minLong = (double)geoPoint.getLongitudeE6()/(double)1E6;
-    		double maxLat  = (double)geoPoint.getLatitudeE6()/(double)1E6;
-    		double maxLong = minLong+(double)mapView.getLongitudeSpan()/(double)1E6;
-    		double minLat  = maxLat-(double)mapView.getLatitudeSpan()/(double)1E6;
+    	if (event.getAction() == MotionEvent.ACTION_DOWN) {
+    	    long thisTime = System.currentTimeMillis();
+    	    if (thisTime - lastTouchTime < 250) {
+    	    	GeoPoint geoPoint = mapView.getProjection().fromPixels(0,0);
+    	    	double minLong = (double)geoPoint.getLongitudeE6()/(double)1E6;
+    	    	double maxLat  = (double)geoPoint.getLatitudeE6()/(double)1E6;
+    	    	double maxLong = minLong+(double)mapView.getLongitudeSpan()/(double)1E6;
+    	    	double minLat  = maxLat-(double)mapView.getLatitudeSpan()/(double)1E6;
 
-    		int minGridLong = (int)(Math.floor(minLong+180));
-    		int minGridLat  = (int)(Math.floor(minLat+90));
-    		int maxGridLong = (int)(Math.ceil(maxLong+180));
-    		int maxGridLat  = (int)(Math.ceil(maxLat+90));
-    		
-    		if (this.curLong != minGridLong || this.curLat != minGridLat) {
-    			MapArea mapArea = new MapArea(minGridLat, maxGridLat, minGridLong, maxGridLong);
+    	    	int minGridLong = (int)(Math.floor(minLong+180));
+    	    	int minGridLat  = (int)(Math.floor(minLat+90));
+    	    	int maxGridLong = (int)(Math.ceil(maxLong+180));
+    	    	int maxGridLat  = (int)(Math.ceil(maxLat+90));
 
-    			Log.i("Map", "minLong=" + minLong + ",maxLong=" + maxLong + ",minLat=" + minLat + ",maxLat=" + maxLat);
-    			Log.i("Map - Grid", "minGridLong=" + minGridLong + ",maxGridLong=" + maxGridLong + ",minGridLat=" + minGridLat + ",maxGridLat=" + maxGridLat);
+    	    	MapArea mapArea = new MapArea(minGridLat, maxGridLat, minGridLong, maxGridLong);
 
-    			ArrayList<OverlayItem> overlayItemList = new ArrayList<OverlayItem>();
+    	    	Log.i("Map", "minLong=" + minLong + ",maxLong=" + maxLong + ",minLat=" + minLat + ",maxLat=" + maxLat);
+    	    	Log.i("Map - Grid", "minGridLong=" + minGridLong + ",maxGridLong=" + maxGridLong + ",minGridLat=" + minGridLat + ",maxGridLat=" + maxGridLat);
 
-    			new PopulateMapArea(mapView, mapArea, this).execute(overlayItemList);
+    	    	ArrayList<OverlayItem> overlayItemList = new ArrayList<OverlayItem>();
 
-    			for (OverlayItem item : overlayItemList) {
-    				this.addOverlay(item);
-    			}
-    			Log.i("airfields", "Item count" + this.mOverlays.size());  
+    	    	new PopulateMapArea(mapView, mapArea, this).execute(overlayItemList);
 
-    			this.curLong = minGridLong;
-    			this.curLat = minGridLat;
+    	    	for (OverlayItem item : overlayItemList) {
+    	    		this.addOverlay(item);
+    	    	}
+    	    	Log.i("airfields", "Item count" + this.mOverlays.size());  
+
+    	    	lastTouchTime = -1;
+    		} else {
+    			lastTouchTime = thisTime;
     		}
         }
 
